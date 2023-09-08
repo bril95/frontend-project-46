c2d84f2b3f6d02740e7c474ef7186f4d44e425b1
bril95 <bril.vadim@mail.ru>
2023-09-08 14:49:24 +0300

    add argument and action in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,15 @@ program.version('0.0.1');
 
 program.description('Compares two configuration files and shows a difference.');
 
+program.arguments('<filepath1> <filepath2>');
+
+program.action((filepath1, filepath2) => {
+  console.log('filepath1:', filepath1);
+  console.log('filepath2:', filepath2);
+});
+
 program.helpOption('-h, --help', 'output usage information');
+
+program.option('-f, --format <type>', 'output format');
 
 program.parse();
