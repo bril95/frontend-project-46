9e696398fb82fddfc6bca4660e6250e1fb38652f
bril95 <bril.vadim@mail.ru>
2023-09-15 11:46:22 +0300

    remove gitignore files

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+// import generateDiff from '../src/index.js';
+// import expected from '../__fixtures__/expected.txt';
+
+// test('reverse', () => {
+//     expect(reverse('hello')).toEqual('olleh');
+//     expect(reverse('')).toEqual('');
+//   });
