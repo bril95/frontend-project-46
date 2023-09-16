a00d460a9fe2529573b41bb1440a018cc9323e1b
bril95 <bril.vadim@mail.ru>
2023-09-16 18:32:28 +0300

    changes fixtures

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import genDiff from './genDiff.js';
 import parseFile from './parsers.js';
 
 const generateDiff = (filepath1, filepath2) => {
-  const readFile1 = parseFile(filepath1);
-  const readFile2 = parseFile(filepath2);
-  return genDiff(readFile1, readFile2);
+  const object1 = parseFile(filepath1);
+  const object2 = parseFile(filepath2);
+  return genDiff(object1, object2);
 };
 
 export default generateDiff;
