12468b5628185af61a6aa2bd4195f505c91a12f0
bril95 <bril.vadim@mail.ru>
2023-09-21 12:54:50 +0300

    add asciinema

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 [![Github Actions](https://github.com/bril95/frontend-project-46/actions/workflows/steps.yml/badge.svg)]()
 [![asciicast](https://asciinema.org/a/kYLZmgyVkfO1dVQ1vyrWq4YrI.svg)](https://asciinema.org/a/kYLZmgyVkfO1dVQ1vyrWq4YrI)
 [![asciicast](https://asciinema.org/a/22yH9ZKxK2i2MVwXxNK3TIo0V.svg)](https://asciinema.org/a/22yH9ZKxK2i2MVwXxNK3TIo0V)
+[![asciicast](https://asciinema.org/a/K5XSoUNMEpLBAtoNQiw8e41De.svg)](https://asciinema.org/a/K5XSoUNMEpLBAtoNQiw8e41De)
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import genDiff from './genDiff.js';
 import parseFile from './parsers.js';
 import stylish from './stylish.js';
 
-const generateDiff = (filepath1, filepath2) => {
+const generateDiff = (filepath1, filepath2, format = 'stylish') => {
   const object1 = parseFile(filepath1);
   const object2 = parseFile(filepath2);
-  return stylish(genDiff(object1, object2));
+  const compareObj = stylish(genDiff(object1, object2));
+  return compareObj;
 };
 
 export default generateDiff;
