2ed1e391b5b0268326bf8b1b2856972b70e9339f
bril95 <bril.vadim@mail.ru>
2023-09-27 14:14:52 +0300

    fix name in stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,31 +1,31 @@
 import _ from 'lodash';
 
-const indent = (depth, defaultSpace = 2) => ' '.repeat(defaultSpace).repeat((depth * 2) - 2);
+const getIndent = (depth, defaultSpace = 2) => ' '.repeat(defaultSpace).repeat((depth * 2) - 2);
 
 const stringify = (obj, depth) => {
   if (!_.isObject(obj)) {
     return obj;
   }
   const objectToString = Object.entries(obj, depth)
-    .map(([key, value]) => `${indent(depth + 1)}    ${key}: ${stringify(value, depth + 1)}`);
-  return `{\n${objectToString.join('\n')}\n${indent(depth + 1)}}`;
+    .map(([key, value]) => `${getIndent(depth + 1)}    ${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${objectToString.join('\n')}\n${getIndent(depth + 1)}}`;
 };
 
 const makeInfo = {
-  nested: (element, depth, iter) => `${indent(depth)}    ${element.key}: ${iter(element.children, depth + 1)}`,
-  unchanged: (element, depth) => `${indent(depth)}    ${element.key}: ${stringify(element.value, depth)}`,
-  deleted: (element, depth) => `${indent(depth)}  - ${element.key}: ${stringify(element.value, depth)}`,
-  added: (element, depth) => `${indent(depth)}  + ${element.key}: ${stringify(element.value, depth)}`,
+  nested: (element, depth, iter) => `${getIndent(depth)}    ${element.key}: ${iter(element.children, depth + 1)}`,
+  unchanged: (element, depth) => `${getIndent(depth)}    ${element.key}: ${stringify(element.value, depth)}`,
+  deleted: (element, depth) => `${getIndent(depth)}  - ${element.key}: ${stringify(element.value, depth)}`,
+  added: (element, depth) => `${getIndent(depth)}  + ${element.key}: ${stringify(element.value, depth)}`,
   updated: (element, depth) => [
-    `${indent(depth)}  - ${element.key}: ${stringify(element.oldValue, depth)}`,
-    `${indent(depth)}  + ${element.key}: ${stringify(element.value, depth)}`,
+    `${getIndent(depth)}  - ${element.key}: ${stringify(element.oldValue, depth)}`,
+    `${getIndent(depth)}  + ${element.key}: ${stringify(element.value, depth)}`,
   ],
 };
 
 const stylish = (tree) => {
   const iter = (node, depth) => {
     const dataToString = node.flatMap((elem) => makeInfo[elem.type](elem, depth, iter));
-    return `{\n${dataToString.join('\n')}\n${indent(depth)}}`;
+    return `{\n${dataToString.join('\n')}\n${getIndent(depth)}}`;
   };
   return iter(tree, 1);
 };
