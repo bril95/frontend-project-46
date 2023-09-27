ee896b4c44eb67719777dee16d770a4690c54c59
bril95 <bril.vadim@mail.ru>
2023-09-27 14:07:03 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+import fs from 'fs';
+import path from 'path';
 import genDiff from './genDiff.js';
 import parseFile from './parsers.js';
 import formatName from './formatters/index.js';
 
+function getWay(filepath) {
+  if (filepath.startsWith('/')) {
+    return path.resolve(filepath);
+  } return `${process.cwd()}/__fixtures__/${filepath}`;
+}
+
+function readFile(filepath) {
+  const readFiles = fs.readFileSync(getWay(filepath), 'utf8');
+  const typeFile = path.extname(filepath);
+  return [readFiles, typeFile];
+}
+
 const generateDiff = (filepath1, filepath2, format = 'stylish') => {
-  const object1 = parseFile(filepath1);
-  const object2 = parseFile(filepath2);
+  const object1 = parseFile(readFile(filepath1));
+  const object2 = parseFile(readFile(filepath2));
   const compareObj = formatName(genDiff(object1, object2), format);
   return compareObj;
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,16 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 
-function getWay(filepath) {
-  if (filepath.startsWith('/')) {
-    return path.resolve(filepath);
-  } return `${process.cwd()}/__fixtures__/${filepath}`;
-}
-
-function parseFile(filepath) {
-  const readFile = fs.readFileSync(getWay(filepath), 'utf8');
-  if (filepath.slice(-4) === 'json') {
-    return JSON.parse(readFile);
-  } return yaml.load(readFile);
+function parseFile([file, typeFile]) {
+  switch (typeFile) {
+    case '.yml':
+      return yaml.load(file);
+    case '.yaml':
+      return yaml.load(file);
+    case '.json':
+      return JSON.parse(file);
+    default:
+      throw new Error(`Unknown format: '${typeFile}'!`);
+  }
 }
 
 export default parseFile;
