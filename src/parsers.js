71f18ebd57613be643bdc662e35dc7e92b156e37
bril95 <bril.vadim@mail.ru>
2023-10-08 11:58:05 +0300

    fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,11 +2,11 @@ import yaml from 'js-yaml';
 
 const parseFile = (data, typeFile) => {
   const objWithTypeFile = {
-    json: JSON.parse(data),
-    yml: yaml.load(data),
-    yaml: yaml.load(data),
+    json: () => JSON.parse(data),
+    yml: () => yaml.load(data),
+    yaml: () => yaml.load(data),
   };
-  return objWithTypeFile[typeFile];
+  return objWithTypeFile[typeFile](data);
 };
 
 export default parseFile;
